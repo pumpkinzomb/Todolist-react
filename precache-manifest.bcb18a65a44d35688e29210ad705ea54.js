6255194e3d25b7a05360af04a08fa31f38f3faec
pumpkinzomb <pumpkinzomb@gmail.com>
2018-12-31 15:15:25 +0900

    Updates

---
+++ precache-manifest.bcb18a65a44d35688e29210ad705ea54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03467ff623a6ad779902",
+    "url": "/todolist-app/static/js/runtime~main.03467ff6.js"
+  },
+  {
+    "revision": "ca3df8cbb65d41404b9e",
+    "url": "/todolist-app/static/js/main.ca3df8cb.chunk.js"
+  },
+  {
+    "revision": "93e5498c018d9ab07733",
+    "url": "/todolist-app/static/js/1.93e5498c.chunk.js"
+  },
+  {
+    "revision": "ca3df8cbb65d41404b9e",
+    "url": "/todolist-app/static/css/main.295496fa.chunk.css"
+  },
+  {
+    "revision": "ebc6eedf35a6f5fe22bd68cbc09c09c1",
+    "url": "/todolist-app/index.html"
+  }
+];
